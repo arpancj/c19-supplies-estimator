dc083a2167296fea16deb14e61ae746d8ae67e6c
Arpan CJ <arpan@able.do>
2020-01-14 19:59:31 +0530

    Add postcss & related libs + tailwindcss

---
+++ source/stylesheets/generate_css.11ty.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+
+module.exports = class {
+  async data() {
+    const rawFilepath = path.join(__dirname, './application.pcss');
+    const rawCss = await fs.readFileSync(rawFilepath);
+
+    return {
+      permalink: 'assets/application.css',
+      rawFilepath,
+      rawCss
+    };
+  }
+
+  async render({ rawCss, rawFilepath }) {
+    return await postcss([
+      require('precss'),
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('cssnano')
+    ])
+      .process(rawCss, { from: rawFilepath })
+      .then(result => result);
+  }
+};
