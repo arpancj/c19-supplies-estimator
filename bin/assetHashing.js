90f1a50dd66d8157a85d247d09c2db48789dcf8c
Arpan CJ <arpan@able.do>
2020-01-17 16:57:49 +0530

    Parse and replace asset urls in HTML & CSS files with hashed versions

--- bin/assetHashing.js
+++ bin/assetHashing.js
@@ -2,17 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const md5 = require('md5');
 const assetsFolder = path.join(__dirname, '..', 'dist', 'assets');
+const htmlFolder = path.join(__dirname, '..', 'dist');
+const cheerio = require('cheerio');
 
-const getAllFiles = function(directory, arrayOfFiles) {
+const filesMap = new Map();
+
+const getAllFiles = function(directory, fileExtensions, arrayOfFiles) {
   let files = fs.readdirSync(directory);
   arrayOfFiles = arrayOfFiles || [];
 
   files.forEach(file => {
     let filePath = path.join(directory, file);
     if (fs.statSync(filePath).isDirectory()) {
-      arrayOfFiles = getAllFiles(filePath, arrayOfFiles);
+      arrayOfFiles = getAllFiles(filePath, fileExtensions, arrayOfFiles);
     } else {
-      arrayOfFiles.push({ directory, file });
+      let extension = path.extname(file).substr(1);
+      if (fileExtensions.includes(extension)) {
+        arrayOfFiles.push({ directory, file });
+      }
     }
   });
 
@@ -48,6 +55,8 @@ const addFileHash = file => {
   file.relativePath = `${relativePath(file)}/${file.file}`;
   file.hashedRelativePath = `${relativePath(file)}/${file.hashedFileName}`;
 
+  filesMap.set(file.relativePath, file.hashedRelativePath);
+
   fs.rename(
     path.join(file.directory, file.file),
     path.join(file.directory, file.hashedFileName),
@@ -62,10 +71,86 @@ const addFileHash = file => {
   return true;
 };
 
-filesArray = getAllFiles(assetsFolder);
+const updateReferencesinHtml = (file, assetFilesArray) => {
+  const filePath = path.join(file.directory, file.file);
+  const $ = cheerio.load(fs.readFileSync(filePath, 'utf-8'));
 
-filesArray.forEach(function(file) {
+  $('link[href]').each(function() {
+    let cssPath = $(this).attr('href');
+    let hashedPath = filesMap.get(cssPath);
+
+    if (hashedPath) {
+      $(this).attr('href', hashedPath);
+    }
+  });
+
+  $('script[src]').each(function() {
+    let jsPath = $(this).attr('src');
+    let hashedPath = filesMap.get(jsPath);
+
+    if (hashedPath) {
+      $(this).attr('src', hashedPath);
+    }
+  });
+
+  $('img[src]').each(function() {
+    let imagePath = $(this).attr('src');
+    let hashedPath = filesMap.get(imagePath);
+
+    if (hashedPath) {
+      $(this).attr('src', hashedPath);
+    }
+  });
+
+  fs.writeFile(filePath, $.html(), err => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    }
+  });
+};
+
+const updateReferencesInCss = (file, assetFilesArray) => {
+  const filePath = path.join(file.directory, file.file);
+  let css = fs.readFileSync(filePath, 'utf-8');
+  regex = /(url\s*\()(\s*)([^\s'")].*?)(\s*\))/gi;
+
+  const urls = css.match(regex);
+
+  urls.forEach(url => {
+    let imagePath = url.slice(4, -1);
+    let hashedPath = filesMap.get(imagePath);
+
+    if (hashedPath) {
+      css = css.replace(imagePath, hashedPath);
+    }
+  });
+
+  fs.writeFile(filePath, css, err => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    }
+  });
+};
+
+assetFilesArray = getAllFiles(assetsFolder, [
+  'js',
+  'css',
+  'svg',
+  'png',
+  'jpg',
+  'woff'
+]);
+
+assetFilesArray.forEach(function(file) {
   addFileHash(file);
 });
 
-console.log(filesArray);
+htmlFilesArray = getAllFiles(htmlFolder, ['html']);
+htmlFilesArray.forEach(function(file) {
+  updateReferencesinHtml(file, assetFilesArray);
+});
+
+cssFilesArray = getAllFiles(htmlFolder, ['css']);
+cssFilesArray.forEach(function(file) {
+  updateReferencesInCss(file, assetFilesArray);
+});
